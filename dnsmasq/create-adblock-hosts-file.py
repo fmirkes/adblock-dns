3134c6d56ffe60ad7dd3c531bf65dad5555983ad
Florian Mirkes <florian.mirkes@mailbox.org>
2021-07-24 16:41:24 +0200

    Remove fetcher header

--- dnsmasq/create-adblock-hosts-file.py
+++ dnsmasq/create-adblock-hosts-file.py
@@ -104,9 +104,7 @@ def fetch_url_content(url):
     
     url_content = ''
     try:
-      url_request = Request(url, headers={
-                          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0"})
-      url = urlopen(url_request)
+      url = urlopen(url)
       
       url_content_charset = url.headers.get_content_charset()
       if url_content_charset is None:
